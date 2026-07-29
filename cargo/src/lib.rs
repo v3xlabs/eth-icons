92d8ee607aebf474d18ed929c5bde2d5acfa95f9
Luc <luc@lucemans.nl>
2026-07-29 22:23:23 +0200

    update pkg

--- cargo/src/lib.rs
+++ cargo/src/lib.rs
@@ -1,3 +1,29 @@
+/*!
+ * `eth-icons` is an icon fetching library for EVM icons.
+ *
+ * ```rust
+ * use eth_icons::{};
+ *
+ * #[tokio::main]
+ * pub async fn main() {
+ *    let client = reqwest::client();
+ * }
+ * ```
+ *
+ * ## BYI Reqwest
+ *
+ * You can also bring your own `reqwest::client`.
+ *
+ * ```rust
+ * use eth_icons::{};
+ *
+ * #[tokio::main]
+ * pub async fn main() {
+ *    let client = reqwest::client();
+ * }
+ * ```
+ */
+
 pub mod client;
 pub mod error;
 pub mod icon;
