3004d10cf43e94d23349f136bf4226d986906a69
418teapotcat <418teapotcat@users.noreply.github.com>
2026-07-30 17:38:00 +0200

    fix: add @types/node, fix tsdown config imports

--- typescript/tsdown.config.ts
+++ typescript/tsdown.config.ts
@@ -1,7 +1,11 @@
 import fs from "node:fs";
 import path from "node:path";
+import url from "node:url";
 
 import { defineConfig } from "tsdown";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const package_ = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "package.json"), "utf8"),
