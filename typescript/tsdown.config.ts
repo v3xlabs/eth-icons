647bfead03038bbaaeb99eb39f6b9ee00c2e526b
418 TPC <418@v3x.email>
2026-07-30 17:41:57 +0200

    Add sourcemaps and license banner (#5)
    Co-authored-by: 418teapotcat <418teapotcat@users.noreply.github.com>

--- typescript/tsdown.config.ts
+++ typescript/tsdown.config.ts
@@ -1,4 +1,15 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+
 import { defineConfig } from "tsdown";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const package_ = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "package.json"), "utf8"),
+);
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
@@ -6,4 +17,12 @@ export default defineConfig({
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
+  sourcemap: true,
+  banner: {
+    js: `/**
+ * @license ${package_.name} v${package_.version}
+ * (c) ${new Date().getFullYear()} ${package_.author}
+ * Released under the ${package_.license} License.
+ */`,
+  },
 });
