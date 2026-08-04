fe238556f2a9cd5fb6a6c7e01f1b1d4747828392
Luc van Kampen <luc@lucemans.nl>
2026-08-04 09:47:12 +0200

    Introduce max_response_bytes, ResponseTooLarge, & redirect documentation (#10)

--- cargo/src/error.rs
+++ cargo/src/error.rs
@@ -2,6 +2,10 @@
 pub enum Error {
     #[error("HTTP error: {0}")]
     HttpError(#[from] reqwest::Error),
+    #[error("JSON error: {0}")]
+    JsonError(#[from] serde_json::Error),
+    #[error("Response body exceeds the {max_bytes}-byte limit")]
+    ResponseTooLarge { max_bytes: usize },
     #[error("Internal error: {0}")]
     InternalError(String),
 }
